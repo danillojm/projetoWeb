a193e65d74a1698229d1b8becf20d8cf525f45b2
Danillo <Danillojm@gmail.com>
2016-04-30 23:07:40 -0300

    iniciando

---
+++ ProjetoWeb/src/main/java/br/com/projetoWeb/model/Pessoa.java
@@ -0,0 +1,7 @@
+package br.com.projetoWeb.model;
+
+public class Pessoa {
+
+	private String nome;
+	 
+}
---
+++ ProjetoWeb/src/main/java/br/com/projetoWeb/model/Usuario.java
@@ -0,0 +1,61 @@
+package br.com.projetoWeb.model;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Table(name ="tb_usuario")
+public class Usuario {
+
+	
+	
+	@Column(length =32, nullable =false)
+	private String senha;
+
+	@Column(nullable=false)
+	private Character tipo;
+	
+	@Column(nullable=false)
+	private boolean ativo;
+
+	@OneToOne
+	@JoinColumn(nullable=false)
+	private Pessoa pessoa;
+	
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Character getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Character tipo) {
+		this.tipo = tipo;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	
+}
+
